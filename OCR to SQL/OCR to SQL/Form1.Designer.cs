b82904f4b4021517a51afec6a2c6906c8ba0e8b5
Cody Neiman <neiman1010222@gmail.com>
2017-11-11 18:02:32 -0500

    Finished OCR detection

--- OCR to SQL/OCR to SQL/Form1.Designer.cs
+++ OCR to SQL/OCR to SQL/Form1.Designer.cs
@@ -32,13 +32,15 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(175, 176);
+            this.button1.Location = new System.Drawing.Point(28, 263);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(152, 67);
+            this.button1.Size = new System.Drawing.Size(152, 70);
             this.button1.TabIndex = 0;
             this.button1.Text = "Begin OCR";
             this.button1.UseVisualStyleBackColor = true;
@@ -46,17 +48,17 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(28, 99);
+            this.textBox1.Location = new System.Drawing.Point(28, 173);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(246, 20);
+            this.textBox1.Size = new System.Drawing.Size(310, 20);
             this.textBox1.TabIndex = 1;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(293, 77);
+            this.button2.Location = new System.Drawing.Point(395, 147);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(161, 63);
+            this.button2.Size = new System.Drawing.Size(152, 70);
             this.button2.TabIndex = 2;
             this.button2.Text = "Browse for Scan Folder";
             this.button2.UseVisualStyleBackColor = true;
@@ -64,7 +66,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(175, 293);
+            this.button3.Location = new System.Drawing.Point(395, 263);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(152, 70);
             this.button3.TabIndex = 3;
@@ -72,18 +74,39 @@
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(395, 26);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(152, 70);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Tesseract-OCR Location";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(28, 52);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(310, 20);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.SkyBlue;
-            this.ClientSize = new System.Drawing.Size(478, 434);
+            this.ClientSize = new System.Drawing.Size(578, 361);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -95,6 +118,8 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
--- OCR to SQL/OCR to SQL/Form1.cs
+++ OCR to SQL/OCR to SQL/Form1.cs
@@ -8,11 +8,15 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Diagnostics;
+using System.IO;
+using Timer = System.Timers.Timer;
 
 namespace OCR_to_SQL
 {
     public partial class Form1 : Form
     {
+        Timer t = new Timer(1000);
+
         public Form1()
         {
             InitializeComponent();
@@ -20,7 +24,33 @@ namespace OCR_to_SQL
 
         private void button1_Click(object sender, EventArgs e)
         {
+            string ex1 = textBox1.Text;
 
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.FileName = textBox2.Text;
+            startInfo.CreateNoWindow = false;
+            startInfo.UseShellExecute = false;         
+            startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            
+            string[] files = Directory.GetFiles(textBox1.Text);
+            //textBox1.Text = files[0];
+
+            for (int i = 0; i < files.Length; i++)
+            {
+                startInfo.Arguments = String.Format("{0} {1}//output{2} hocr", files[i], ex1, i + 1);
+                
+                try
+                {
+                    using (Process exeProcess = Process.Start(startInfo))
+                    {
+                        exeProcess.WaitForExit();
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("Error running Tesseract-OCR");
+                }
+            }
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -41,6 +71,41 @@ namespace OCR_to_SQL
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fbd2 = new OpenFileDialog();
+            if (fbd2.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                textBox2.Text = fbd2.FileName;
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string tesseractPath = "C:\\Program Files (x86)\\Tesseract-OCR\\tesseract.exe";
+            if (File.Exists(tesseractPath))
+            {
+                textBox2.Text = tesseractPath;
+            }
+            else
+            {
+                t.Elapsed += new System.Timers.ElapsedEventHandler(t_Elapsed);
+                t.Start();
+            }
+
+        }
+
+        private void t_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            t.Stop();
+            MessageBox.Show("Tesseract-OCR installation could not be found!\nPlease manually enter the path to tesseract.exe!");
         }
     }
 }
